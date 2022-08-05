a3c134680255f082872869e060296331218de3ba
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2022-08-05 17:44:03 +0200

    Updated package metadata

--- src/Plugin.cc
+++ src/Plugin.cc
@@ -10,10 +10,10 @@ zeek::plugin::Configuration Plugin::Configure()
 	zeek::plugin::Configuration config;
 
 	config.name = "IoT::mDNS";
-	config.description = "Multicast DNS (mDNS) Protocol Analyzer for Zeek IDS";
+	config.description = "Multicast DNS (mDNS) package for Zeek";
 
-	config.version.major = 0;
-	config.version.minor = 1;
+	config.version.major = 1;
+	config.version.minor = 0;
 	config.version.patch = 0;
 
 	return config;
